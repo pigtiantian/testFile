0d3fe0713e3687d04a6d2ff96d950c2a439dc66e
zhangshaotian <1125173070@qq.com>
2020-06-29 15:52:58 +0800

    新增bos.vue

---
+++ src/bos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{msg}}
+        {{hi}}
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'zhangshaotian',
+            hi: 'hello'
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
