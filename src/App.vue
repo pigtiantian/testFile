a0dd2a5648de820cc2d291b197444a26cb3bd8cc
zhangshaotian <1125173070@qq.com>
2020-06-29 15:48:18 +0800

    say hi

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,19 @@
-
+<template>
+    <div>
+        {{msg}}
+        {{hi}}
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'hellobike2016',
+            hi: 'hi'
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
